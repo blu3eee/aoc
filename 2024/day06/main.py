6a766c83c7a3a27d59b6c5727924b50669880014
blu3eee <jackismyname7@gmail.com>
2024-12-07 17:57:35 -0800

    2024: day6 part 1 & 2 brute force

--- 2024/day06/main.py
+++ 2024/day06/main.py
@@ -10,19 +10,104 @@ def read_input(file):
 def write_output(file, data):
     with open(file, 'w') as f:
         f.write(data)
+
+directions = '^>v<'
+dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+def part1(data):    
+    map = [list(line) for line in data]
+    n, m = len(map), len(map[0])
     
+    cx, cy = 0, 0
+    dir = '^'
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] in directions:
+                cx, cy = i, j
+                dir = map[i][j]
+                break
+    
+    seen = set()
+    while cx in range(n) and cy in range(m):
+        seen.add((cx, cy))
+        nx = cx + dirs[directions.index(dir)][0]
+        ny = cy + dirs[directions.index(dir)][1]
+        if nx in range(n) and ny in range(m) and map[nx][ny] == '#':
+            dir = directions[(directions.index(dir) + 1) % 4]
+        else:
+            cx, cy = nx, ny
 
-def convert_data(data):
-    pass
+    return len(seen)
 
-def part1(data):
-    pass
+def is_looped(nmap: list[list[str]], cpos: tuple[int, int], dir: str) -> bool:
+    map = nmap.copy()
+    n, m = len(map), len(map[0])
 
-def part2_convert_data(data):
-    pass
+    cx, cy = cpos
+    steps = {}
+
+    prev_dir = dir
+    while cx in range(n) and cy in range(m):
+        nx = cx + dirs[directions.index(dir)][0]
+        ny = cy + dirs[directions.index(dir)][1]
+        
+        if nx in range(n) and ny in range(m) and map[nx][ny] == '#':
+            dir = directions[(directions.index(dir) + 1) % 4]
+        else:
+            cx, cy = nx, ny
+
+        if prev_dir != dir:
+            if (cx, cy) in steps and dir in steps[(cx, cy)]:
+                return True
+            
+        prev_dir = dir
+        steps[(cx, cy)] = [dir] if (cx, cy) not in steps else steps[(cx, cy)] + [dir]
+
+    return False
 
 def part2(data):
-    pass
+    g = [list(line) for line in data]
+    n, m = len(g), len(g[0])
+    
+    ix, iy = 0, 0
+    cd = 0
+    for i in range(len(g)):
+        for j in range(len(g[i])):
+            if g[i][j] in directions:
+                ix, iy = i, j
+                break
+    
+    g2 = [[y for y in r] for r in g]
+    ans = 0
+    for ox in range (n):
+        for oy in range(m):
+            if g[ox][oy] == '#' or g[ox][oy] == '^':
+                continue
+            g[ox][oy] = '#'
+            
+            seen = set()
+            cd = 0
+            cx, cy = ix, iy
+            while cx in range(n) and cy in range(m) and (cx,cy,cd) not in seen:
+                seen.add((cx,cy,cd))
+                while True:
+                    cdir = dirs[cd]
+                    nx, ny = cx + cdir[0], cy + cdir[1]
+                    if nx in range(n) and ny in range(m) and g[nx][ny] == '#':
+                        cd = (cd + 1) % 4
+                    else:
+                        cx, cy = nx, ny
+                        break
+            
+            if (cx, cy, cd) in seen:
+                print('got', cx, cy, cd)
+                ans += 1
+                g2[ox][oy] = '?'
+
+            g[ox][oy] = '.'  
+            
+    print(g2)
+    return ans
 
 def main():
     test_file = 'test.txt'
@@ -32,7 +117,7 @@ def main():
     print("---- Part 1 ----")
     test_input = read_input(test_file)
     test_output = part1(test_input)
-    part1_expected_test_output = 0
+    part1_expected_test_output = 41
     if test_output == part1_expected_test_output:
         print('Test passed')
     else:
@@ -48,7 +133,7 @@ def main():
 
     # part 2
     print("---- Part 2 ----")
-    part2_expected_test_output = 0
+    part2_expected_test_output = 6
     test_output = part2(test_input)
     if test_output == part2_expected_test_output:
         print('Test passed')
@@ -58,8 +143,8 @@ def main():
         print('Got:', test_output)
         return
     
-    output = part2(input)
-    print('Part 2 Output:', output)
+    # output = part2(input)
+    # print('Part 2 Output:', output)
     
 
 if __name__ == '__main__':
