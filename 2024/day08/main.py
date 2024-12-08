b3e08482226ec2488d614a51a5259f1fed52e5ae
blu3eee <jackismyname7@gmail.com>
2024-12-07 21:27:15 -0800

    2024: day8 init

---
+++ 2024/day08/main.py
@@ -0,0 +1,54 @@
+
+def read_input(file):
+    # read all the lines in the file and return them as a list
+    with open(file, 'r') as f:
+        data = f.readlines()
+    # strip the newline character from the end of each line
+    data = [line.strip() for line in data]
+    return data
+
+def write_output(file, data):
+    with open(file, 'w') as f:
+        f.write(data)
+    
+test_file = 'test.txt'
+
+input_file = 'input.txt'
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+# part 1
+print("---- Part 1 ----")
+test_input = read_input(test_file)
+test_output = part1(test_input)
+part1_expected_test_output = 0
+if test_output == part1_expected_test_output:
+    print('Test passed')
+    input = read_input(input_file)
+    print(test_input)
+    output = part1(input)
+    print('Part 1 Output:', output)
+else:
+    print('Test failed')
+    print('Expected:', part1_expected_test_output)
+    print('Got:', test_output)
+
+# part 2
+print("---- Part 2 ----")
+part2_expected_test_output = 0
+test_output = part2(test_input)
+if test_output == part2_expected_test_output:
+    print('Test passed')
+    output = part2(input)
+    print('Part 2 Output:', output)
+else:
+    print('Test failed')
+    print('Expected:', part2_expected_test_output)
+    print('Got:', test_output)
+    
+
